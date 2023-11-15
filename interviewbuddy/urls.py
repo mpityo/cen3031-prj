c153829a9b4f8726292a7d445f1f9814ded6668c
omaralkadry <omar.alkadry@ufl.edu>
2023-11-14 19:31:28 -0500

    adjust home page

--- interviewbuddy/urls.py
+++ interviewbuddy/urls.py
@@ -5,5 +5,6 @@ from .views import LoginView, RegisterView, home_view
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('', home_view, name='home'),
     path('home/', home_view, name='home')
 ]
